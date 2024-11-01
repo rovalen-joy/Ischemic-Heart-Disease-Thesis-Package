c54e327ca5cd95bd9ff291d3e8599f5a53502a89
Rovalen Joy Calaguing <rovalen.calaguing@wvsu.edu.ph>
2024-11-01 21:53:15 +0800

    Fixed the firebase configuration

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -9,14 +9,14 @@ import { getFirestore } from 'firebase/firestore'
 // Your web app's Firebase configuration
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBw6vjBqYF9j5fOdokLlOYdiuSRNfasN2A",
-  authDomain: "ihd-prediction-system-a73dc.firebaseapp.com",
-  databaseURL: "https://ihd-prediction-system-a73dc-default-rtdb.firebaseio.com",
-  projectId: "ihd-prediction-system-a73dc",
-  storageBucket: "ihd-prediction-system-a73dc.firebasestorage.app",
-  messagingSenderId: "851265186412",
-  appId: "1:851265186412:web:31b83985a25f1b70118649",
-  measurementId: "G-BVL22YDQP9"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
